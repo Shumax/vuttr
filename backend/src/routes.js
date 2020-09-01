195165c2fed1bdf4f63b9d5fc1323d3be1ebb605
Shumax <maxcastro.mv@gmail.com>
2020-09-01 20:33:20 -0300

    Authorization

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const ToolController = require('./app/controllers/ToolController.js');
 const UserController = require('./app/controllers/UserController.js');
+const authToken = require('./app/middlewares/auth.js');
 
 const routes = Router();
 
 routes.post('/register', UserController.storage);
 routes.post('/login', UserController.signin);
-routes.get('/tools', ToolController.index);
-routes.post('/tools', ToolController.storage);
-routes.delete('/tools/:id', ToolController.destroy);
+routes.get('/tools', authToken, ToolController.index);
+routes.post('/tools', authToken, ToolController.storage);
+routes.delete('/tools/:id', authToken, ToolController.destroy);
 
 module.exports = routes;
